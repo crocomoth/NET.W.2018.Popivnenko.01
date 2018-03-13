96b0d6ec6f5c760a2f0ac53c19f15bce454189e3
crocomoth <justletmeplaymf@gmail.com>
2018-03-13 23:14:02 +0300

    TestProj added
    also assembly created

---
+++ NET.W.2018.Popivnenko.01.TestProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.W._2018.Popivnenko._01.TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
